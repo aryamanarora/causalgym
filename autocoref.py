2235a8e6fa9518eb4bb64512533539b212382ab3
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-10-17 13:11:38 -0700

    batch processing

--- autocoref.py
+++ autocoref.py
@@ -6,10 +6,12 @@ import os
 from tqdm import tqdm
 import torch
 import pandas as pd
-from plotnine import ggplot, aes, facet_wrap, facet_grid, geom_bar, theme, element_text, geom_errorbar, ggtitle
-from plotnine.scales import scale_color_manual
+from plotnine import ggplot, aes, facet_wrap, facet_grid, geom_bar, theme, element_text, geom_errorbar, ggtitle, geom_hline, geom_point
+from plotnine.scales import scale_color_manual, scale_x_log10
 import argparse
 import scipy.stats as stats
+
+names = ["Participant 1", "Participant 2"]
 
 def binomial_confidence_interval(count, total, confidence=0.95):
     """Calculate a binomial confidence using Wilson score."""
@@ -31,35 +33,43 @@ def autocoref():
     # load coref model
     nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe(
-    "fastcoref", 
-    config={'model_architecture': 'LingMessCoref', 'model_path': 'biu-nlp/lingmess-coref', 'device': 'cuda:0' if torch.cuda.is_available() else 'cpu'}
+        "fastcoref", 
+        config={'model_architecture': 'LingMessCoref', 'model_path': 'biu-nlp/lingmess-coref', 'device': 'cuda:0' if torch.cuda.is_available() else 'cpu'}
     )
 
     final = {}
 
     # for each file
-    for file in tqdm(glob.glob("logs/*.json")):
+    for file in glob.glob("logs/*.json"):
         with open(file, 'r') as f:
+            print(file)
 
             # load data
             data = json.load(f)
             res = {}
 
             # run coref
-            for key in data:
+            for key in tqdm(data):
                 if key == 'metadata': continue
                 res[key] = {}
+                res[key]['results'] = []
                 res[key]['counts'] = data[key]['counts']
                 res[key]['counts_resolved'] = {option: 0 for option in data[key]['counts']}
                 res[key]['counts_resolved_pronoun'] = {option: 0 for option in data[key]['counts']}
 
-                for sent in data[key]['sentences']:
-                    # resolve coref
-                    doc = nlp(
-                        sent,
-                        component_cfg={"fastcoref": {'resolve_text': True}}
-                    )
+                # batch process
+                docs = nlp.pipe(
+                    [sent for sent in data[key]['sentences']],
+                    component_cfg={"fastcoref": {'resolve_text': True}}
+                )
+
+                # get metrics
+                for i, doc in enumerate(docs):
                     resolved = doc._.resolved_text
+                    res[key]['results'].append({
+                        "text": data[key]['sentences'][i],
+                        "resolved": resolved,
+                    })
                     for option in data[key]['counts']:
                         res[key]['counts_resolved'][option] += (1 if option in '.'.join(resolved.split('.')[1:]) else 0)
                         res[key]['counts_resolved_pronoun'][option] += (1 if resolved.split('. ')[1].startswith(option) else 0)
@@ -93,17 +103,21 @@ def plot():
         for sent in data[key]:
             for metric in data[key][sent]:
                 for option in data[key][sent][metric]:
+                    i = 0 if sent.startswith(option) else 1
                     count = data[key][sent][metric][option]
                     lower, upper = binomial_confidence_interval(count, 100) if metric in ["counts", "counts_resolved_pronoun"] else [None, None]
+
                     rows.append({
                         "model": model,
+                        "type": model.split('-')[0],
                         "is_human": False,
                         "sent": sent,
                         "metric": metric,
-                        "option": option,
+                        "option": names[i],
                         "count": count,
                         "lower": lower,
-                        "upper": upper
+                        "upper": upper,
+                        "params": params
                     })
     
     # read stimuli data
@@ -112,15 +126,18 @@ def plot():
 
     for stimulus in stimuli:
         for option in stimulus['human']:
+            i = 0 if stimulus['text'].startswith(option) else 1
             rows.append({
                 "model": "human",
+                "type": "human",
                 "is_human": True,
                 "sent": stimulus['text'],
                 "metric": "counts_resolved_pronoun",
-                "option": option,
+                "option": names[i],
                 "count": stimulus['human'][option] * 100,
                 "lower": None,
-                "upper": None
+                "upper": None,
+                "params": None
             })
     
     # df, set model order
@@ -129,6 +146,9 @@ def plot():
     df = pd.DataFrame(rows)
     df['model'] = pd.Categorical(df['model'].astype(str))
     df['model'].cat.set_categories(order, inplace=True)
+    df['sent'] = df['sent'].map(lambda x: '\n'.join([x[i:i+20] for i in range(0, len(x), 20)]))
+    df['sent'] = pd.Categorical(df['sent'].astype(str))
+    df['sent'].cat.set_categories(['\n'.join([x['text'][i:i+20] for i in range(0, len(x['text']), 20)]) for x in sorted(stimuli, key=lambda x: list(x['human'].values())[0])], inplace=True)
 
     # plot
     plot = (ggplot(df[df['model'] != 'human'], aes(x="model", y="count", fill="option"))
@@ -141,13 +161,15 @@ def plot():
     df_pron["prob"] = df_pron["count"].map(lambda x: x / 100)
 
     # plot
-    plot = (ggplot(df_pron, aes(x="model", y="prob", fill="option", color="is_human"))
+    plot = (ggplot(df_pron[df_pron['model'] != 'human'], aes(x="params", y="prob", fill="type"))
             + scale_color_manual(values=["#0000FF00", "black"])
-            + geom_bar(stat="identity")
-            + geom_errorbar(aes(ymin="lower", ymax="upper"), width=0.2, color="black")
+            + geom_errorbar(aes(ymin="lower", ymax="upper"), alpha=0.5, width=0.05, color="black")
+            + geom_point(stat="identity")
+            + scale_x_log10()
             + facet_grid("option~sent", scales='free_y')
-            + theme(figure_size=(15, 6), axis_text_x=element_text(rotation=45, hjust=1))
-            + ggtitle("What does 'He' resolve to?"))
+            + theme(figure_size=(25, 6), axis_text_x=element_text(rotation=45, hjust=1))
+            + ggtitle("What does '(S)he' resolve to?")
+            + geom_hline(df_pron[df_pron['model'] == 'human'], aes(yintercept="prob"), linetype="dashed", show_legend=True))
     plot.save("logs/overall/plot_pron.pdf")
 
     # now do just counts
@@ -155,12 +177,12 @@ def plot():
     df_counts["prob"] = df_counts["count"].map(lambda x: x / 100)
 
     # plot
-    plot = (ggplot(df_counts, aes(x="model", y="prob", fill="option", color="is_human"))
+    plot = (ggplot(df_counts[df_counts['model'] != 'human'], aes(x="model", y="prob", fill="option"))
             + scale_color_manual(values=["#0000FF00", "black"])
             + geom_bar(stat="identity")
             + geom_errorbar(aes(ymin="lower", ymax="upper"), width=0.2, color="black")
             + facet_grid("option~sent", scales='free_y')
-            + theme(figure_size=(15, 6), axis_text_x=element_text(rotation=45, hjust=1))
+            + theme(figure_size=(25, 6), axis_text_x=element_text(rotation=45, hjust=1))
             + ggtitle("Is a participant mentioned by name?"))
     plot.save("logs/overall/plot_counts.pdf")
 
