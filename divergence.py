e5124a737134bfa2e48eb54c6028616558693b2f
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-10-23 18:28:58 -0700

    partition by sentence template

--- divergence.py
+++ divergence.py
@@ -20,8 +20,9 @@ def load_data():
         names.extend([(name, gender) for name in stimuli['names'][gender]])
     
     # construct all possible sentences
-    sentences = []
+    sentences = {}
     for i, stimulus in enumerate(stimuli['sentences']):
+        sentences[i] = []
         for name1 in names:
             subbed = stimulus.replace("<name1>", name1[0])
             for name2 in names:
@@ -33,7 +34,7 @@ def load_data():
                     referents = []
                     if name1[1] == gender: referents.append(name1[0])
                     if name2[1] == gender: referents.append(name2[0])
-                    sentences.append({
+                    sentences[i].append({
                         "sent": subbed3,
                         "match_name1": name1[0] in referents,
                         "match_name2": name2[0] in referents,
@@ -65,42 +66,43 @@ def main(m: str, sentences: list=None):
         model = AutoModelForCausalLM.from_pretrained(m, torch_dtype=torch.bfloat16).to(device)
 
         # generate next token distributions
-        distribs = []
-        sents = [x['sent'] for x in sentences]
-        for batch in tqdm(range(0, len(sents), 200)):
-            inputs = tokenizer(sents[batch:batch+200], return_tensors="pt", padding=True).to(device)
-            logits = model(**inputs).logits.to("cpu")
-            probs = logsoftmax(logits)
-            for i in range(probs.shape[0]):
-                distrib = probs[i, inputs['attention_mask'][i] == 1][-1]
-                distribs.append(distrib)
-        
-        # free memory
-        del model
+        for key in sentences:
+            distribs = []
+            sents = [x['sent'] for x in sentences[key]]
+            for batch in tqdm(range(0, len(sents), 200)):
+                inputs = tokenizer(sents[batch:batch+200], return_tensors="pt", padding=True).to(device)
+                logits = model(**inputs).logits.to("cpu")
+                probs = logsoftmax(logits)
+                for i in range(probs.shape[0]):
+                    distrib = probs[i, inputs['attention_mask'][i] == 1][-1]
+                    distribs.append(distrib)
+            
+            # free memory
+            del model
 
-        # get kl divergence between distributions
-        for _ in tqdm(range(100)):
-            for __ in range(100):
-                i = random.randint(0, len(sentences)-1)
-                j = random.randint(0, len(sentences)-1)
-                if i == j: continue
-                kldiv = torch.nn.functional.kl_div(distribs[i], distribs[j], reduction="sum", log_target=True)
-                label = [sentences[i]["match_name1"], sentences[i]["match_name2"], sentences[j]["match_name1"], sentences[j]["match_name2"]]
-                label = "".join(["T" if x else "F" for x in label])
-                kldivs.append({
-                    "sent1": sents[i],
-                    "sent2": sents[j],
-                    "same": label,
-                    "first": label[:2],
-                    "second": label[2:],
-                    "model": m,
-                    "pronoun_gender1": sentences[i]["pronoun_gender"],
-                    "pronoun_gender2": sentences[j]["pronoun_gender"],
-                    "kldiv": kldiv.item()
-                })
+            # get kl divergence between distributions
+            for _ in tqdm(range(100)):
+                for __ in range(100):
+                    i = random.randint(0, len(sentences)-1)
+                    j = random.randint(0, len(sentences)-1)
+                    if i == j: continue
+                    kldiv = torch.nn.functional.kl_div(distribs[i], distribs[j], reduction="sum", log_target=True)
+                    label = [sentences[i]["match_name1"], sentences[i]["match_name2"], sentences[j]["match_name1"], sentences[j]["match_name2"]]
+                    label = "".join(["T" if x else "F" for x in label])
+                    kldivs.append({
+                        "sent1": sents[i],
+                        "sent2": sents[j],
+                        "same": label,
+                        "first": label[:2],
+                        "second": label[2:],
+                        "model": m,
+                        "pronoun_gender1": sentences[i]["pronoun_gender"],
+                        "pronoun_gender2": sentences[j]["pronoun_gender"],
+                        "kldiv": kldiv.item()
+                    })
     
     # dump
-    with open(f"logs/{m.replace('/', '-')}.json", "w") as f:
+    with open(f"logs/new/{m.replace('/', '-')}.json", "w") as f:
         json.dump(kldivs, f)
 
 if __name__ == "__main__":
