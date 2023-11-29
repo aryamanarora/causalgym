cd4cdfa162d8ab1ab8e85652a4f1e6e56cbd78bd
Aryaman Arora <aryaman.arora2020@gmail.com>
2023-11-29 15:32:28 -0800

    update script

--- das.py
+++ das.py
@@ -2,47 +2,20 @@ import torch
 import csv
 import random
 from utils import MODELS, WEIGHTS, Sentence, get_options, make_sentence
-from transformers import (
-    AutoTokenizer,
-    AutoModelForCausalLM,
-    get_linear_schedule_with_warmup,
-)
+from transformers import AutoTokenizer, AutoModelForCausalLM, get_linear_schedule_with_warmup
 from torch.nn import CrossEntropyLoss
 import sys
 import pandas as pd
 from tqdm import tqdm
 import argparse
-from plotnine import (
-    ggplot,
-    geom_point,
-    geom_label,
-    geom_tile,
-    geom_smooth,
-    aes,
-    facet_wrap,
-    theme,
-    element_text,
-    facet_grid,
-    geom_bar,
-    geom_hline,
-    scale_y_log10,
-    ggtitle,
-)
+from plotnine import ggplot, geom_point, aes, facet_grid, geom_line, ggtitle
 from plotnine.scales import scale_x_continuous, scale_fill_cmap, scale_y_reverse
-from typing import List
 from utils import names
 import argparse
 
 # add align-transformers to path
 sys.path.append("../align-transformers/")
-from models.utils import (
-    embed_to_distrib,
-    top_vals,
-    format_token,
-    lsm,
-    sm,
-    count_parameters,
-)
+from models.utils import format_token, sm, count_parameters
 from models.configuration_alignable_model import (
     AlignableRepresentationConfig,
     AlignableConfig,
@@ -53,9 +26,6 @@ from models.interventions import (
     RotatedSpaceIntervention,
     BoundlessRotatedSpaceIntervention,
 )
-from models.gpt_neox.modelings_alignable_gpt_neox import create_gpt_neox
-from umap import UMAP
-
 
 def rotated_space_intervention(num_dims):
     def func(args, proj_dim):
@@ -77,12 +47,12 @@ def intervention_config(model_type, intervention_type, layer, num_dims):
                 1,  # max number of unit
             ),
         ],
-        alignable_interventions_type=BoundlessRotatedSpaceIntervention,
+        alignable_interventions_type=BoundlessRotatedSpaceIntervention if num_dims == -1 else rotated_space_intervention(num_dims),
     )
     return alignable_config
 
 
-def experiment(model="EleutherAI/pythia-70m", steps=1000):
+def experiment(model="EleutherAI/pythia-70m", steps=1000, num_dims=-1):
     # load model
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     tokenizer = AutoTokenizer.from_pretrained(model)
@@ -90,9 +60,7 @@ def experiment(model="EleutherAI/pythia-70m", steps=1000):
     gpt = AutoModelForCausalLM.from_pretrained(
         model,
         revision="main",
-        torch_dtype=WEIGHTS.get(model, torch.bfloat16)
-        if device == "cuda:0"
-        else torch.float32,
+        torch_dtype=WEIGHTS.get(model, torch.bfloat16) if device == "cuda:0" else torch.float32,
     ).to(device)
 
     def make_pair():
@@ -101,12 +69,9 @@ def experiment(model="EleutherAI/pythia-70m", steps=1000):
         he = random.choice(names[gender1])
         she = random.choice(names[gender2])
         completions = [
-            "is tired",
-            "went home",
-            "walked",
-            "ran",
-            "works there",
-            "joined the army",
+            "is tired", "is excited", "is ready", "went home", "walked", "is walking",
+            "ran", "is running", "works there", "joined the army", "plays soccer",
+            "likes playing games", "said no to me"
         ]
         completion = random.choice(completions)
         pair = (
@@ -135,161 +100,170 @@ def experiment(model="EleutherAI/pythia-70m", steps=1000):
     for layer_i in [0]:
         # for pos_i in range(1, len(base.input_ids[0])):
         for pos_i in [1]:
-            # how many dims to intervene on
-            for num_dims in [1]:
-                print(layer_i, pos_i, num_dims)
+            # set up alignable model
+            alignable_config = intervention_config(
+                type(gpt), "block_output", layer_i, num_dims
+            )
+            alignable = AlignableModel(alignable_config, gpt)
+            alignable.set_device(device)
+            alignable.disable_model_gradients()
 
-                # set up alignable model
-                alignable_config = intervention_config(
-                    type(gpt), "block_output", layer_i, num_dims
-                )
-                alignable = AlignableModel(alignable_config, gpt)
-                alignable.set_device(device)
-                alignable.disable_model_gradients()
-
-                # optimizer
-                t_total = steps
-                warm_up_steps = 0.1 * t_total
-                optimizer_params = []
-                for k, v in alignable.interventions.items():
-                    optimizer_params += [{"params": v[0].rotate_layer.parameters()}]
+            # optimizer
+            t_total = steps
+            warm_up_steps = 0.1 * t_total
+            optimizer_params = []
+            for k, v in alignable.interventions.items():
+                optimizer_params += [{"params": v[0].rotate_layer.parameters()}]
+                try:
                     optimizer_params += [
                         {"params": v[0].intervention_boundaries, "lr": 1e-2}
                     ]
+                except:
+                    pass
 
-                print("model trainable parameters: ", count_parameters(alignable.model))
-                print(
-                    "intervention trainable parameters: ", alignable.count_parameters()
-                )
+            print("model trainable parameters: ", count_parameters(alignable.model))
+            print(
+                "intervention trainable parameters: ", alignable.count_parameters()
+            )
 
-                optimizer = torch.optim.Adam(optimizer_params, lr=1e-3)
-                scheduler = get_linear_schedule_with_warmup(
-                    optimizer,
-                    num_warmup_steps=warm_up_steps,
-                    num_training_steps=t_total,
-                )
+            optimizer = torch.optim.Adam(optimizer_params, lr=1e-3)
+            scheduler = get_linear_schedule_with_warmup(
+                optimizer,
+                num_warmup_steps=warm_up_steps,
+                num_training_steps=t_total,
+            )
 
-                gradient_accumulation_steps = 4
-                total_step = 0
-                temperature_start = 50.0
-                temperature_end = 0.1
-                temperature_schedule = (
-                    torch.linspace(temperature_start, temperature_end, t_total)
-                    .to(torch.bfloat16)
-                    .to(device)
-                )
-                alignable.set_temperature(temperature_schedule[total_step])
+            gradient_accumulation_steps = 4
+            total_step = 0
+            temperature_start = 50.0
+            temperature_end = 0.1
+            temperature_schedule = (
+                torch.linspace(temperature_start, temperature_end, t_total)
+                .to(torch.bfloat16)
+                .to(device)
+            )
+            alignable.set_temperature(temperature_schedule[total_step])
 
-                def calculate_loss(logits, label, step):
-                    shift_logits = logits[..., :, :].contiguous()
-                    loss_fct = CrossEntropyLoss()
-                    shift_logits = shift_logits[0, -1].softmax(-1)
-                    shift_labels = torch.tensor(label)
-                    shift_labels = shift_labels.to(shift_logits.device)
-                    loss = loss_fct(shift_logits, shift_labels)
+            def calculate_loss(logits, label, step):
+                shift_logits = logits[..., :, :].contiguous()
+                loss_fct = CrossEntropyLoss()
+                shift_logits = shift_logits[0, -1].softmax(-1)
+                shift_labels = torch.tensor(label)
+                shift_labels = shift_labels.to(shift_logits.device)
+                loss = loss_fct(shift_logits, shift_labels)
 
-                    if step >= warm_up_steps:
+                if step >= warm_up_steps:
+                    try:
                         for k, v in alignable.interventions.items():
                             boundary_loss = 1.0 * v[0].intervention_boundaries.sum()
                         loss += boundary_loss
+                    except:
+                        pass
 
-                    return loss
+                return loss
 
-                iterator = tqdm(range(t_total))
-                for step in iterator:
-                    # make pair
-                    pair, label, _ = make_pair()
+            iterator = tqdm(range(t_total))
+            for step in iterator:
+                # make pair
+                pair, label, _ = make_pair()
 
-                    # inference
-                    _, counterfactual_outputs = alignable(
-                        pair[0],
-                        [pair[1]],
-                        {"sources->base": ([[[pos_i]]], [[[pos_i]]])},
-                    )
+                # inference
+                _, counterfactual_outputs = alignable(
+                    pair[0],
+                    [pair[1]],
+                    {"sources->base": ([[[pos_i]]], [[[pos_i]]])},
+                )
 
-                    # loss and backprop
-                    loss = calculate_loss(
-                        counterfactual_outputs.logits, tokenizer.encode(label)[0], step
-                    )
-                    loss_str = round(loss.item(), 2)
+                # loss and backprop
+                loss = calculate_loss(
+                    counterfactual_outputs.logits, tokenizer.encode(label)[0], step
+                )
+                loss_str = round(loss.item(), 2)
 
-                    # print stats
-                    stats = {"loss": loss_str}
-                    distrib = sm(counterfactual_outputs.logits)[0, -1]
-                    for tok in tokens:
-                        prob = distrib[tok].item()
-                        stats[format_token(tokenizer, tok)] = f"{prob:.3f}"
-                    hidden_state_size = gpt.config.hidden_size
+                # print stats
+                stats = {"loss": loss_str}
+                distrib = sm(counterfactual_outputs.logits)[0, -1]
+                for tok in tokens:
+                    prob = distrib[tok].item()
+                    stats[format_token(tokenizer, tok)] = f"{prob:.3f}"
+                hidden_state_size = gpt.config.hidden_size
+                try:
                     for k, v in alignable.interventions.items():
                         stats[
                             "bound"
                         ] = f"{v[0].intervention_boundaries.sum() * v[0].embed_dim:.3f}"
-                    iterator.set_postfix(stats)
+                except:
+                    pass
+                iterator.set_postfix(stats)
 
-                    # gradient accumulation
-                    if gradient_accumulation_steps > 1:
-                        loss = loss / gradient_accumulation_steps
-                    if total_step % gradient_accumulation_steps == 0:
-                        if not (gradient_accumulation_steps > 1 and total_step == 0):
-                            loss.backward()
-                            optimizer.step()
-                            scheduler.step()
-                            alignable.set_zero_grad()
-                            alignable.set_temperature(temperature_schedule[total_step])
+                # gradient accumulation
+                if gradient_accumulation_steps > 1:
+                    loss = loss / gradient_accumulation_steps
+                if total_step % gradient_accumulation_steps == 0:
+                    if not (gradient_accumulation_steps > 1 and total_step == 0):
+                        loss.backward()
+                        optimizer.step()
+                        scheduler.step()
+                        alignable.set_zero_grad()
+                        alignable.set_temperature(temperature_schedule[total_step])
 
-                    # eval
-                    if step % 20 == 0:
-                        with torch.no_grad():
-                            boundary = None
+                # eval
+                if step % 20 == 0:
+                    with torch.no_grad():
+                        boundary = torch.tensor(1.0)
+                        try:
                             for k, v in alignable.interventions.items():
                                 boundary = (
                                     v[0].intervention_boundaries.sum() * v[0].embed_dim
                                 )
-                            boundary = boundary.item()
+                        except:
+                            pass
+                        boundary = boundary.item()
 
-                            for pair, label, base_label in evalset:
-                                _, counterfactual_outputs = alignable(
-                                    pair[0],
-                                    [pair[1]],
-                                    {"sources->base": ([[[pos_i]]], [[[pos_i]]])},
+                        for pair, label, base_label in evalset:
+                            _, counterfactual_outputs = alignable(
+                                pair[0],
+                                [pair[1]],
+                                {"sources->base": ([[[pos_i]]], [[[pos_i]]])},
+                            )
+                            loss = calculate_loss(
+                                counterfactual_outputs.logits,
+                                tokenizer.encode(label)[0],
+                                step
+                            )
+                            distrib = sm(counterfactual_outputs.logits)[0, -1]
+                            for tok in tokens:
+                                prob = distrib[tok].item()
+                                stats[format_token(tokenizer, tok)] = f"{prob:.3f}"
+                                data.append(
+                                    {
+                                        "step": step,
+                                        "src_label": label,
+                                        "base_label": base_label,
+                                        "label": label + " > " + base_label,
+                                        "loss": loss.item(),
+                                        "token": format_token(tokenizer, tok),
+                                        "prob": prob,
+                                        "bound": boundary,
+                                    }
                                 )
-                                loss = calculate_loss(
-                                    counterfactual_outputs.logits,
-                                    tokenizer.encode(label)[0],
-                                    step
-                                )
-                                distrib = sm(counterfactual_outputs.logits)[0, -1]
-                                for tok in tokens:
-                                    prob = distrib[tok].item()
-                                    stats[format_token(tokenizer, tok)] = f"{prob:.3f}"
-                                    data.append(
-                                        {
-                                            "step": step,
-                                            "src_label": label,
-                                            "base_label": base_label,
-                                            "label": label + " > " + base_label,
-                                            "loss": loss.item(),
-                                            "token": format_token(tokenizer, tok),
-                                            "prob": prob,
-                                            "bound": boundary,
-                                        }
-                                    )
 
-                    total_step += 1
+                total_step += 1
 
     # print plots
     df = pd.DataFrame(data)
 
     plot = (
         ggplot(df, aes(x="step", y="bound"))
-        + geom_smooth(method="loess")
+        + geom_line()
         + ggtitle("intervention boundary")
     )
     plot.save("figs/bound.pdf")
 
     plot = (
         ggplot(df, aes(x="step", y="loss", color="factor(label)"))
-        + geom_smooth(method="loess")
+        + geom_point(alpha=0.1)
+        + geom_line(stat='summary', fun_y=lambda x: x.mean())
         + ggtitle("per-label loss")
     )
     plot.save("figs/loss.pdf")
@@ -297,7 +271,8 @@ def experiment(model="EleutherAI/pythia-70m", steps=1000):
     plot = (
         ggplot(df, aes(x="step", y="prob", color="factor(label)"))
         + facet_grid("~token")
-        + geom_smooth(method="loess")
+        + geom_point(alpha=0.1)
+        + geom_line(stat='summary', fun_y=lambda x: x.mean())
         + ggtitle("per-label probs")
     )
     plot.save("figs/prob.pdf")
@@ -326,9 +301,10 @@ def experiment(model="EleutherAI/pythia-70m", steps=1000):
                 )
 
                 logits = counterfactual_outputs.logits[0, -1]
+                probs = logits.softmax(dim=-1)
                 for token in tokens:
                     print(
-                        f"{pos_i:<5} {format_token(tokenizer, test.input_ids[0][pos_i]):<15} {format_token(tokenizer, token):<15} {logits[token].item() - base_logits[i][token]:>9.4f}"
+                        f"{pos_i:<5} {format_token(tokenizer, test.input_ids[0][pos_i]):<15} {format_token(tokenizer, token):<15} {logits[token].item() - base_logits[i][token]:>9.4f}  {probs[token].item():>9.4%}"
                     )
             print()
 
@@ -347,7 +323,8 @@ def experiment(model="EleutherAI/pythia-70m", steps=1000):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="EleutherAI/pythia-70m")
-    parser.add_argument("--steps", type=int, default=1000)
+    parser.add_argument("--steps", type=int, default=5000)
+    parser.add_argument("--num_dims", type=int, default=-1)
     args = parser.parse_args()
     experiment(args.model, args.steps)
 
