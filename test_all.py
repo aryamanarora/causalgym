0173c9b2df88054203ea4aa7de442c635fb8dfa7
Aryaman Arora <aryaman.arora2020@gmail.com>
2024-01-04 17:16:14 -0500

    test everything

---
+++ test_all.py
@@ -0,0 +1,14 @@
+import os
+from datasets import list_datasets
+
+def run_command(dataset, method):
+    steps = 125
+    command = f"python das.py --model EleutherAI/pythia-70m --intervention {method} --dataset {dataset} --position each --num-tokens 1 --num-dims 1 --steps {steps}"
+    os.system(command)
+
+def main():
+    methods = ["das", "probe_sklearn"]
+    datasets = [d for d in list_datasets() if d.startswith("syntaxgym/")]
+    for dataset in datasets:
+        for method in methods:
+            run_command(dataset, method)
