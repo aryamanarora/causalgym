2cb13d228cf738c9ec374ae2d7647df2fbab6d6c
Aryaman Arora <aryaman.arora2020@gmail.com>
2024-01-15 15:32:23 -0800

    paired acc in benchmark func

--- benchmark.py
+++ benchmark.py
@@ -39,47 +39,42 @@ def benchmark(model=None, task=None, debug=False):
 
         # make data
         for dataset in datasets:
-            trainset, labels = make_data(tokenizer, dataset, 4, 50, -1, device)
+            trainset, _ = make_data(tokenizer, dataset, 4, 100, -1, device)
             count, correct = 0, 0
             probs_base, probs_src = [], []
 
-            for (pair, src_label, base_label, pos_base, _, _) in tqdm(trainset):
-                for pair_i in range(2):
-                    # inference
-                    output = gpt(**pair[pair_i])
-                    logits = get_last_token(output.logits, pair[0].attention_mask)
+            for (pair, src_label, base_label, _, _, _) in tqdm(trainset):
+                # inference
+                base_output = gpt(**pair[0])
+                src_output = gpt(**pair[1])
+                base_logits = get_last_token(base_output.logits, pair[0].attention_mask)
+                src_logits = get_last_token(src_output.logits, pair[1].attention_mask)
 
-                    # accuracy
-                    for i in range(4):
-                        probs = torch.softmax(logits[i], dim=-1)
-                        if probs[base_label[i]] > probs[src_label[i]]:
-                            correct += 1
-                        if debug:
-                            print(probs[base_label[i]] > probs[src_label[i]])
-                            print(tokenizer.decode(pair[pair_i].input_ids[i]))
-                            print(tokenizer.decode(pair[1 - pair_i].input_ids[i]))
-                            top_vals(tokenizer, probs, highlight=[base_label[i], src_label[i]])
-                            input()
-                        probs_base.append(probs[base_label[i]].item())
-                        probs_src.append(probs[src_label[i]].item())
+                # check for batch accuracy
+                for i in range(4):
+                    base_probs = torch.softmax(base_logits[i], dim=-1)
+                    src_probs = torch.softmax(src_logits[i], dim=-1)
+                    if base_probs[base_label[i]] > base_probs[src_label[i]] and src_probs[src_label[i]] > src_probs[base_label[i]]:
+                        correct += 1
+                    if debug:
+                        print(base_probs[base_label[i]] > base_probs[src_label[i]] and src_probs[src_label[i]] > src_probs[base_label[i]])
+                        print(tokenizer.decode(pair[0].input_ids[i]))
+                        print(tokenizer.decode(pair[1].input_ids[i]))
+                        top_vals(tokenizer, base_probs, highlight=[base_label[i], src_label[i]])
+                        top_vals(tokenizer, src_probs, highlight=[base_label[i], src_label[i]])
+                        input()
 
-                        count += 1
-
-                    # swap labels
-                    src_label, base_label = base_label, src_label
+                    count += 1
 
             # store stats
             data.append({
                 "dataset": dataset,
                 "count": count,
                 "correct": correct,
-                "probs_base": sum(probs_base) / len(probs_base),
-                "probs_src": sum(probs_src) / len(probs_src),
                 "iia": correct / count,
                 "parameters": gpt.num_parameters(),
             })
-            print(f"{dataset:<30} {correct / count:>10.2%} ({correct}/{count})")
-            print(f"{dataset:<30} {sum(probs_base) / len(probs_base):>10.2%} {sum(probs_src) / len(probs_src):>10.2%}")
+            print(f"{dataset:<30} {correct / count:>10.2%} ({correct} / {count})")
     
     # save data
     with open("logs/benchmark.json", "w") as f:
